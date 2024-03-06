346632aeeb00c5d263a93d43306d32a51ac16f70
Christophe Wang <christophewang@Christophes-MacBook-Pro.local>
2024-03-06 13:56:59 +0100

    get .env data

--- dags/AFP.py
+++ dags/AFP.py
@@ -1,12 +1,19 @@
 import requests
 import pymongo
 import datetime
+import os
+from dotenv import load_dotenv
+# Load environment variables from .env file
+load_dotenv()
 
 # get Apple's stock price as well as rating from FMP
 def get_stock_price():
-    url = ("https://financialmodelingprep.com/api/v3/quote/AAPL?'")
+    url = "https://financialmodelingprep.com/api/v3/quote/AAPL?'"
+
+    apikey = os.environ.get("API_FMP")  # Retrieve the apikey from the environment variable
+
     payload = {
-        "apikey": "",
+        "apikey": apikey,
     }
     response = requests.get(url, params=payload)
     data = response.json()
